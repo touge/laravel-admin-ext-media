8abf3ab41f4b33842a38a42e2bbd9f89d48be867
NickYuan <nick@pc-06372.sh.the9.com>
2019-02-19 12:39:23 +0800

    change elfinder prefix-> config(admin.route.prefix)

--- src/Media.php
+++ src/Media.php
@@ -24,6 +24,7 @@ class Media extends Field
     {
         $name = $this->formatName($this->column);
         $baseURL = config('filesystems.disks.public.url');
+        $backend_prefix = config('admin.route.prefix');
 
         $this->variables = [
             'baseURL'=>$baseURL,
@@ -35,7 +36,7 @@ var BASE_URL= "{$baseURL}";
 $(".select-elfinder-file").on('click', function (event) {
     event.preventDefault();
     var _id = $(this).data("id")
-    var elfinderUrl = "/elfinder/popup/" + _id;
+    var elfinderUrl = "/{$backend_prefix}/elfinder/popup/" + _id;
     openwindow(elfinderUrl, '选择文件', 850, 450);
 });
 EOT;
